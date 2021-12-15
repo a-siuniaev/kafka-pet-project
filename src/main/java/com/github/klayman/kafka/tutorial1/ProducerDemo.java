6c12103d4a1cf3f5958abb65e2baa4b80fee9119
klay-man <glinko1567@gmail.com>
2021-12-15 14:04:01 +0300

    Simple producer added

--- src/main/java/com/github/klayman/kafka/tutorial1/ProducerDemo.java
+++ src/main/java/com/github/klayman/kafka/tutorial1/ProducerDemo.java
@@ -1,11 +1,36 @@
 package com.github.klayman.kafka.tutorial1;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class ProducerDemo {
     public static void main(String[] args) {
         // create Producer properties
+        Properties properties = new Properties();
+
+        //hard-coded way to create properties
+        //properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+            // this is how kafka now how to serialize messages to bytes
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
         // create Producer
+            // producer with kay and value as String, String
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
 
-        // send data
+        // create a producer record
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "hello world");
+
+
+        // send data - async!
+        producer.send(record);
+
+        producer.flush();
+        producer.close();
     }
 }

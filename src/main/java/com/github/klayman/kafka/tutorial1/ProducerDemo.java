918438b672a5ae0693f6aa71191e7a1317faa6bb
klay-man <glinko1567@gmail.com>
2021-12-15 13:08:02 +0300

    Initial commit

---
+++ src/main/java/com/github/klayman/kafka/tutorial1/ProducerDemo.java
@@ -0,0 +1,11 @@
+package com.github.klayman.kafka.tutorial1;
+
+public class ProducerDemo {
+    public static void main(String[] args) {
+        // create Producer properties
+
+        // create Producer
+
+        // send data
+    }
+}
